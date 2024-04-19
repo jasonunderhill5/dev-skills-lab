7f77561aa9cbca834f92a3d5541e36441376eca3
jasonunderhill5 <jasonunderhill4005@gmail.com>
2024-04-18 23:05:27 -0700

    set up env files and data

---
+++ models/skills.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const skillSchema = new Schema({
+  text: String,
+  difficulty: String,
+  done: Boolean,
+})
+
+const Skill = mongoose.model('Skill', skillSchema)
+
+export {
+  Skill
+}
---
+++ routes/skills.js
@@ -0,0 +1,9 @@
+import { Router } from "express"
+import { skills } from '../data/skill-data.js'
+import * as skillsCtrl from '../controllers/skills.js'
+
+const router = Router()
+
+router.get('/', skillsCtrl.index)
+
+export { router }
